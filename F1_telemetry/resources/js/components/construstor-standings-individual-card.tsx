d5d0835b6d961da0f4401fcfc210ca25bf913b21
25Jburton <25jburton@gmail.com>
2025-05-09 23:39:11 -0400

    updates to team standing component

--- F1_telemetry/resources/js/components/construstor-standings-individual-card.tsx
+++ F1_telemetry/resources/js/components/construstor-standings-individual-card.tsx
@@ -1,43 +1,106 @@
 import { Button } from '@/components/ui/button';
 import { type ConstructorsStandingItemType } from '@/types';
 import {Drawer,DrawerClose,DrawerContent,DrawerDescription,DrawerFooter,DrawerHeader,DrawerTitle,DrawerTrigger,} from "@/components/ui/drawer"
+import {Dialog,DialogContent,DialogDescription,DialogFooter,DialogHeader,DialogTitle,DialogTrigger} from "@/components/ui/dialog"
+import { Label } from "@/components/ui/label"
+import { Icon } from './icon';
+import {Trophy } from 'lucide-react';
+export function ConstructorStandingsIndividualCard({ standings, year }: { standings: ConstructorsStandingItemType[], year: string }) {
+	return (
+		<div className="p-4 grid auto-rows-min gap-4 md:grid-cols-3">
+			{standings && standings.map((item) => (
+				<div className="p-4 border-sidebar-border/70 dark:border-sidebar-border relative rounded-xl border text-center">
+					<Dialog>
+						<DialogTrigger asChild>
+							<Button variant="ghost"><Label className="text-right">{item['position']}</Label> {item['team']['teamName']}</Button>
+						</DialogTrigger>
+						<DialogContent className="sm:max-w-[425px]">
+							<DialogHeader>
+								<DialogTitle>{item['team']['teamName']} - {item['team']['country']}</DialogTitle>
+								<DialogDescription>
+									General Standings Information {year}
+								</DialogDescription>
+							</DialogHeader>
+							<div className="grid gap-4 py-4">
+								<DialogDescription>
+									Team Information
+								</DialogDescription>
+								<div className="grid grid-cols-6 items-center gap-4">
+									<Label htmlFor="name" className="text-right">
+										Position
+									</Label>
+									<span> 
+										<p className='mb-1'>{item['position']} </p>
+									</span>
 
-export function ConstructorStandingsIndividualCard({ standings }: { standings: ConstructorsStandingItemType[] }) {
-    return (
-        <div className="p-4 grid auto-rows-min gap-4 md:grid-cols-3">
-            {standings && standings.map((item) => (
-                <div className="p-4 border-sidebar-border/70 dark:border-sidebar-border relative rounded-xl border text-center">
-                    <Drawer>
-                        <DrawerTrigger className="p-4 rounded-xl hover:bg-red-500 hover:text-accent-foreground ">{item['position']} - {item['team']['teamName']}</DrawerTrigger>
-                        <DrawerContent>
-                            <DrawerHeader className='text-center'>
-                                <DrawerTitle><h1>{item['position']} - {item['team']['teamName']}</h1></DrawerTitle>
-                                <DrawerDescription>
-                                    General Constructors standings information.
-                                </DrawerDescription>
-                            </DrawerHeader>
-                            <div className='text-center p-4 grid auto-rows-min gap-4 md:grid-cols-4 w-[100vw]'>
-                                <span>
-                                    <p className='mb-1'>Position: {item['position']} </p><br/><p className='mb-1'> Points: {item['points']} </p><br/> Wins: {item['wins']}</span>
-                                <span>
-                                    <p className='mb-1'>Team: {item['team']['teamName']} </p><br/> Nationality: {item['team']['country']}
-                                </span>
-                                {item['team']['constructorsChampionships'] && <span><p className='mb-1'>Constructors Championships: {item['team']['constructorsChampionships']} </p><br/> Drivers Championships: {item['team']['driversChampionships']}</span>}
-                                <span >
-                                    <a href={item['team']['url']} target='blank'>
-                                        <Button variant="outline" className='text-green-500'>More Team Information</Button>
-                                    </a>
-                                </span>
-                            </div>
-                            <DrawerFooter>
-                            <DrawerClose>
-                                <Button variant="outline" className='text-red-500'>Cancel</Button>
-                            </DrawerClose>
-                            </DrawerFooter>
-                        </DrawerContent>
-                    </Drawer>
-                </div>
-            ))}
-        </div>
-    );
+									<Label htmlFor="name" className="text-right">
+										Points
+									</Label>
+									<span>
+										<p className='mb-1'>{item['points']} </p>
+									</span>
+
+									<Label htmlFor="name" className="text-right">
+										Wins
+									</Label>
+									<span>
+										<p className='mb-1'>{parseInt(item['wins']) > 0 ? item['wins'] : 0} </p>
+									</span>
+								</div>
+								<div className="grid grid-cols-4 items-center gap-4">
+									<Label htmlFor="name" className="text-center">
+										Constructors Championships
+										<br/>
+										<p className='mb-1 p-4'>
+											{parseInt(item['team']['constructorsChampionships']) > 0 ? item['team']['constructorsChampionships'] : 0} 
+										</p>
+									</Label>
+									<span> 
+										<p className='mb-1 p-4'>
+											{(() => {
+												const elements = [];
+												for (let i = 0; i < parseInt(item['team']['constructorsChampionships']); i++) {
+													elements.push(<Icon iconNode={Trophy}></Icon>);
+												}
+												return elements;
+											})()}
+										</p>
+									</span>
+
+									<Label htmlFor="name" className="text-center">
+										Drivers Championships
+										<br/>
+										<p className='mb-1 p-4'>
+											{parseInt(item['team']['driversChampionships']) > 0 ? item['team']['driversChampionships'] : 0} 
+										</p>
+									</Label>
+									<span> 
+										<p className='mb-1 p-4'>
+											{(() => {
+												const elements = [];
+												for (let i = 0; i < parseInt(item['team']['driversChampionships']); i++) {
+													elements.push(<Icon iconNode={Trophy}></Icon>);
+												}
+												return elements;
+											})()}
+										</p>
+									</span>
+								</div>
+								<DialogDescription>
+									First Appearance {item['team']['firstAppareance']}
+								</DialogDescription>
+								<div className="grid grid-cols-1 items-center gap-4 text-center">
+									<span>
+										<a href={item['team']['url']} target='blank'>
+											<Button variant="outline" className='text-green-500'>More Team Information</Button>
+										</a>
+									</span>
+								</div>
+							</div>
+						</DialogContent>
+					</Dialog>
+				</div>
+			))}
+		</div>
+	);
 }
--- F1_telemetry/resources/js/components/driver-standings-individual-card.tsx
+++ F1_telemetry/resources/js/components/driver-standings-individual-card.tsx
@@ -1,8 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { type DriversStandingItemType } from '@/types';
-import {Drawer,DrawerClose,DrawerContent,DrawerDescription,DrawerFooter,DrawerHeader,DrawerTitle,DrawerTrigger,} from "@/components/ui/drawer"
 import {Dialog,DialogContent,DialogDescription,DialogFooter,DialogHeader,DialogTitle,DialogTrigger} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 export function DriverStandingsIndividualCard({ standings, year }: { standings: DriversStandingItemType[], year:string }) {
 	return (
@@ -10,71 +8,69 @@ export function DriverStandingsIndividualCard({ standings, year }: { standings: 
 			{standings && standings.map((item) => (
 				<div className="p-4 border-sidebar-border/70 dark:border-sidebar-border relative rounded-xl border text-center">
 				<Dialog>
-				<DialogTrigger asChild>
-					<Button variant="ghost">{item['position']} - {item['driver']['name']} {item['driver']['surname']}</Button>
-				</DialogTrigger>
-				<DialogContent className="sm:max-w-[425px]">
-					<DialogHeader>
-					<DialogTitle>{item['driver']['name']} {item['driver']['surname']} - {item['driver']['nationality']}</DialogTitle>
-					<DialogDescription>
-						General Standings Information {year}
-					</DialogDescription>
-					</DialogHeader>
-					<div className="grid gap-4 py-4">
-						<DialogDescription>
-							Driver Information
-						</DialogDescription>
-						<div className="grid grid-cols-6 items-center gap-4">
-							<Label htmlFor="name" className="text-right">
-								Position
-							</Label>
-							<span> 
-								<p className='mb-1'>{item['position']} </p>
-							</span>
+					<DialogTrigger asChild>
+						<Button variant="ghost"><Label className="text-right">{item['position']}</Label> {item['driver']['name']} {item['driver']['surname']}</Button>
+					</DialogTrigger>
+					<DialogContent className="sm:max-w-[425px]">
+						<DialogHeader>
+							<DialogTitle>{item['driver']['name']} {item['driver']['surname']} - {item['driver']['nationality']}</DialogTitle>
+							<DialogDescription>
+								General Standings Information {year}
+							</DialogDescription>
+						</DialogHeader>
+						<div className="grid gap-4 py-4">
+							<DialogDescription>
+								Driver Information
+							</DialogDescription>
+							<div className="grid grid-cols-6 items-center gap-4">
+								<Label className="text-right">
+									Position
+								</Label>
+								<span> 
+									<p className='mb-1'>{item['position']} </p>
+								</span>
 
-							<Label htmlFor="name" className="text-right">
-								Points
-							</Label>
-							<span>
-								<p className='mb-1'>{item['points']} </p>
-							</span>
+								<Label className="text-right">
+									Points
+								</Label>
+								<span>
+									<p className='mb-1'>{item['points']} </p>
+								</span>
 
-							<Label htmlFor="name" className="text-right">
-								Wins
-							</Label>
-							<span>
-								<p className='mb-1'>{parseInt(item['wins']) > 0 ? item['wins'] : 0} </p>
-							</span>
+								<Label className="text-right">
+									Wins
+								</Label>
+								<span>
+									<p className='mb-1'>{parseInt(item['wins']) > 0 ? item['wins'] : 0} </p>
+								</span>
+							</div>
+							<div className="grid grid-cols-1 items-center gap-4 text-center">
+								<span>
+									<a href={item['driver']['url']} target='blank'>
+										<Button variant="outline" className='text-green-500 mb-3'>More Driver Information</Button>
+									</a>
+								</span>
+							</div>
+							<DialogDescription>
+								Teams Information
+							</DialogDescription>
+							<div className="grid grid-cols-1 items-center gap-4">
+								<span> 
+									<p className='mb-1'>{item['team']['teamName']} </p>
+								</span>
+								<span>
+									<p className='mb-1'>{item['team']['country']} </p>
+								</span>
+							</div>
+							<div className="grid grid-cols-1 items-center gap-4 text-center">
+								<span>
+									<a href={item['team']['url']} target='blank'>
+										<Button variant="outline" className='text-green-500'>More Team Information</Button>
+									</a>
+								</span>
+							</div>
 						</div>
-						<div className="grid grid-cols-1 items-center gap-4 text-center">
-							<span>
-								<a href={item['driver']['url']} target='blank'>
-									<Button variant="outline" className='text-green-500 mb-3'>More Driver Information</Button>
-								</a>
-							</span>
-						</div>
-						<DialogDescription>
-							Teams Information
-						</DialogDescription>
-						<div className="grid grid-cols-1 items-center gap-4">
-							<span> 
-								<p className='mb-1'>{item['team']['teamName']} </p>
-							</span>
-							<span>
-								<p className='mb-1'>{item['team']['country']} </p>
-							</span>
-						</div>
-						<div className="grid grid-cols-1 items-center gap-4 text-center">
-							<span>
-								<a href={item['team']['url']} target='blank'>
-									<Button variant="outline" className='text-green-500'>More Team Information</Button>
-								</a>
-							</span>
-						</div>
-					</div>
-					<DialogFooter>
-					</DialogFooter>
-				</DialogContent>
+					</DialogContent>
 				</Dialog>
 				</div>
 			))}
