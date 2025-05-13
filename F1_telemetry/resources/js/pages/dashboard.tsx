80d4bad9aa6d6c0ee17da96976e1fa8e5f6423b5
25Jburton <25jburton@gmail.com>
2025-05-12 22:49:39 -0400

    adding css to dashboard page

--- F1_telemetry/resources/js/pages/dashboard.tsx
+++ F1_telemetry/resources/js/pages/dashboard.tsx
@@ -5,6 +5,7 @@ import { Head } from '@inertiajs/react';
 import { Icon } from '@/components/ui/icon';
 import { CarIcon, InfoIcon, LogsIcon, MapIcon, PersonStandingIcon, TrophyIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import '../../css/dashboard.css';
 
 const breadcrumbs: BreadcrumbItem[] = [
 	{
@@ -19,75 +20,60 @@ export default function Dashboard() {
 		<AppLayout breadcrumbs={breadcrumbs}>
 			<Head title="Dashboard" />
 			<div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-				<div className="m-4 border-sidebar-border/70 dark:border-sidebar-border relative min-h-[10vh] flex-1 rounded-xl md:min-h-min auto-rows-min gap-4 grid lg:grid-cols-2">
+				<div className="m-4 border-sidebar-border/70 dark:border-sidebar-border relative min-h-[10vh] flex-1 rounded-xl md:min-h-min auto-rows-min gap-4 parent-card grid lg:grid-cols-2">
 					
-					<Card className='p-4'>
-						<CardHeader>
-							<Icon iconNode={PersonStandingIcon}></Icon>
-							<CardTitle className='text-center'>Drivers </CardTitle>
-							<CardDescription className='text-center'>Collection of all Drivers to compete in Formula 1. Basic details for driver based on selection or filtered search.</CardDescription>
-							<a href='/drivers' className='text-center'>
-								<Button variant="destructive">
-									View Drivers
-								</Button>
-							</a>
-						</CardHeader>
+					<Card className='parent-card'>
+						<a href='/drivers' className='text-center'>
+							<CardHeader className='parent-card-drivers'>
+								<Icon iconNode={PersonStandingIcon}></Icon>
+								<CardTitle className='text-center'>Drivers </CardTitle>
+								<CardDescription className='text-center'>Collection of all Drivers to compete in Formula 1. Basic details for driver based on selection or filtered search.</CardDescription>
+							</CardHeader>
+						</a>
 					</Card>
 
-					<Card className='p-4'>
-						<CardHeader>
-							<Icon iconNode={CarIcon}></Icon>
-							<CardTitle className='text-center'>Teams </CardTitle>
-							<CardDescription className='text-center'>Collection of all Teams to compete in Formula 1. Basic details for team based on selection or filtered search.</CardDescription>
-							<a href='/teams' className='text-center'>
-								<Button variant="destructive">
-									View Teams
-								</Button>
-							</a>
-						</CardHeader>
+					<Card className='parent-card'>
+						<a href='/teams' className='text-center'>
+							<CardHeader className='parent-card-teams'>
+								<Icon iconNode={CarIcon}></Icon>
+								<CardTitle className='text-center'>Teams </CardTitle>
+								<CardDescription className='text-center'>Collection of all Teams to compete in Formula 1. Basic details for team based on selection or filtered search.</CardDescription>
+							</CardHeader>
+						</a>
 					</Card>
 
-					<Card className='p-4'>
-						<CardHeader>
-							<Icon iconNode={MapIcon}></Icon>
-							<CardTitle className='text-center'>Circuits </CardTitle>
-							<CardDescription className='text-center'>Information for all tracks utilized throughout the history of Formula 1. Displays basic track length, location, total corners and more.</CardDescription>
-							<a href='/circuits' className='text-center'>
-								<Button variant="destructive">
-									View Circuits
-								</Button>
-							</a>
-						</CardHeader>
+					<Card className='parent-card'>
+						<a href='/circuits' className='text-center'>
+							<CardHeader className='parent-card-circuits'>
+								<Icon iconNode={MapIcon}></Icon>
+								<CardTitle className='text-center'>Circuits </CardTitle>
+								<CardDescription className='text-center'>Information for all tracks utilized throughout the history of Formula 1. Displays basic track length, location, total corners and more.</CardDescription>
+							</CardHeader>
+						</a>
 					</Card>
 
-					<Card className='p-4'>
-						<CardHeader>
-							<Icon iconNode={LogsIcon}></Icon>
-							<CardTitle className='text-center'>Standings </CardTitle>
-							<CardDescription className='text-center'>Current & Historic Standings, Drivers & Constructors. Displays points along with driver & team basic information.</CardDescription>
-							<a href='/standings' className='text-center'>
-								<Button variant="destructive">
-									View Standings
-								</Button>
-							</a>
-						</CardHeader>
+					<Card className='parent-card'>
+						<a href='/standings'  className='text-center' title='View Standings Page'>
+							<CardHeader className='parent-card-standings'>
+								<Icon iconNode={LogsIcon}></Icon>
+								<CardTitle className='text-center'>Standings </CardTitle>
+								<CardDescription className='text-center'>Current & Historic Standings, Drivers & Constructors. Displays points along with driver & team basic information.</CardDescription>
+							</CardHeader>
+						</a>
 					</Card>
 
-					<Card className='p-4'>
-						<CardHeader>
-							<Icon iconNode={TrophyIcon}></Icon>
-							<CardTitle className='text-center'>Results</CardTitle>
-							<CardDescription className='text-center'>Current & Historic Results Based on Session & Year</CardDescription>
-							<a href='/results'  className='text-center'>
-								<Button variant="destructive">
-									View Results
-								</Button>
-							</a>
-						</CardHeader>
+					<Card className='p-4 parent-card'>
+						<a href='/results'  className='text-center' title='View Results Page'>
+							<CardHeader className='parent-card-results'>
+								<Icon iconNode={TrophyIcon}></Icon>
+								<CardTitle className='text-center'>Results</CardTitle>
+								<CardDescription className='text-center'>Current & Historic Results Based on Session & Year</CardDescription>
+							</CardHeader>
+						</a>
 					</Card>
 
-					<Card className='p-4'>
-						<CardHeader>
+					<Card className='p-4 parent-card'>
+						<CardHeader className='parent-card-api'>
 							<Icon iconNode={InfoIcon}></Icon>
 							<CardTitle className='text-center'>API Information </CardTitle>
 							<CardDescription className='text-center'>
